9fa846acff1ca86f8beaa62651ab7038cecbdf2c
YuriSparda <yuri.sparda@hotmail.com>
2022-08-30 17:11:25 -0300

    .

---
+++ proj log/teste.ts
@@ -0,0 +1,14 @@
+
+
+const objPessoa:{nome:String,idade:Number,endereco:{rua:String , numero: Number,complemento:String,bairro:String}}={
+  nome:"yuri",
+  idade: 24,
+  endereco:{
+    rua: " ",
+    numero: 123,
+    complemento: " ",
+    bairro: " ",
+}
+
+};
+    
