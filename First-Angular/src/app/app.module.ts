46bde6b894257d948cfccb9cf2e9376c03701b78
YuriSparda <95309127+YuriSparda@users.noreply.github.com>
2022-09-09 08:37:14 -0300

    test

--- First-Angular/src/app/app.module.ts
+++ First-Angular/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './Templates/header/header.component';
+import { SideNavComponent } from './Templates/side-nav/side-nav.component';
+import { FooterComponent } from './Templates/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
